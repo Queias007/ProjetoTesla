c075ff29e844923c10b8ee89466c33ed39eede3c
Queias007 <miqueiassr09@gmail.com>
2024-08-27 09:16:40 -0300

    adicionando imagens readme

---
+++ scripts.js
@@ -0,0 +1,28 @@
+let formulario = document.querySelector(".formulario")
+
+console.log(formulario)
+
+
+function aparecerFormulario(){
+    formulario.style.left = "37%"
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-100%"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
